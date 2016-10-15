4c5d0174f78d973ba3c9bbfc087bb287eb9d38ad
JWizerd <jwodke@outlook.com>
2016-10-15 12:56:43 -0600

    set up sidebar dynamically add posts through wordpress loop

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,31 @@
 <div class="col-md-3">
-  <p>SUP I'M THE SIDEBAR. THE DATE IS <?php echo the_current_year(); ?></p>
+  <?php define( 'WP_USE_THEMES', false ); ?>
+
+    <!-- Start the Loop. -->
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+   <!-- Display the Title as a link to the Post's permalink. -->
+
+   <dl>
+     <dt>
+       <p><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
+     </dt>
+     <dd>
+       <!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
+      <small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
+     </dd>
+   </dl>
+
+   <!-- Stop The Loop (but note the "else:" - see next line). -->
+
+  <?php endwhile; else : ?>
+
+
+   <!-- The very first "if" tested to see if there were any Posts to -->
+   <!-- display.  This "else" part tells what do if there weren't any. -->
+   <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+
+
+   <!-- REALLY stop The Loop. -->
+  <?php endif; ?>
 </div>
