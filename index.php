2dbff5c6b1e11ebee26cc7f8ac6b3adf09c363ef
JWizerd <jwodke@outlook.com>
2016-10-15 22:02:03 -0600

    fixed home page layout wider coumbs better spacing between boxes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <main class="container-fluid content-wrapper">
   <div class="row">
-    <div class="col-sm-8 col-sm-offset-2">
+    <div class="col-sm-10 col-sm-offset-1">
       <div class="row">
         <div class="col-md-8">
         <h1>HOME PAGE</h1>
