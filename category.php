3054b2c9fb59002f8078319a95d060a8321b74e1
JWizerd <jwodke@outlook.com>
2016-10-17 17:21:52 -0600

    created category template partials

--- category.php
+++ category.php
@@ -1,37 +1,3 @@
-<?php
-/*
-Template Name: Search Page
-*/
-?>
-
 <?php get_header(); ?>
-
-<main class="container-fluid content-wrapper">
-  <div class="row">
-    <div class="col-sm-10 col-sm-offset-1">
-      <div class="row">
-        <div class="col-md-8">
-        <ul>
-          <?php echo '<h1>Category Results:' . ' ' . get_the_category() . '</h1>'; ?>
-
-          <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-            <li class="search-result">
-              <p><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></p>
-              <p>Published <?php echo get_the_date(); ?></p>
-              <small><?php echo wp_trim_words( get_the_content(), 50, '...' ); ?></small>
-            </li>
-
-          <?php endwhile; ?>
-          <?php else: _e( 'Sorry, no posts matched your criteria.', 'textdomain' ); ?>
-          <?php endif; ?>
-        </ul>
-        </div>
-        <?php get_sidebar(); ?>
-      </div>
-    </div>
-  </div>
-
-</main>
-
+<?php get_template_part('category/layout') ?>
 <?php get_footer(); ?>
--- category/loop.php
+++ category/loop.php
@@ -1,14 +1,11 @@
-<?php echo '<h1>Search Results:' . ' ' . get_search_query() . '</h1>'; ?>
-
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
   <li class="search-result">
-    <!-- get the thumbnail for a specific post by grabbing the id and setting the size -->
     <?php echo get_the_post_thumbnail(get_the_ID(), 'large') ?>
     <p><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></p>
+    <p>Published <?php echo get_the_date(); ?></p>
     <small><?php echo wp_trim_words( get_the_content(), 50, '...' ); ?></small>
   </li>
-  <hr>
 
 <?php endwhile; ?>
 <?php else: _e( 'Sorry, no posts matched your criteria.', 'textdomain' ); ?>
