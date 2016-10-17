02fdf5c29d8663f765b0c96788ba6f541be1f291
JWizerd <jwodke@outlook.com>
2016-10-17 17:25:18 -0600

    removed unnecessary js code in footer.php

--- footer.php
+++ footer.php
@@ -36,16 +36,6 @@
     </div>
   </div>
 </footer>
-
-<script>
-  (function($) {
-
-    $('.hideable').on('click', function() {
-      $(this).hide();
-    })
-
-  })( jQuery );
-</script>
 <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
