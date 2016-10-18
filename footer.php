66b72b13df36a012f4c99b396a3b2b6bf2020c30
JWizerd <jwodke@outlook.com>
2016-10-18 17:09:56 -0600

    removed unnecessary footer script in footer

--- footer.php
+++ footer.php
@@ -1,14 +1,13 @@
 <?php wp_footer(); ?>
 
-<footer class="container-fluid content-wrapper">
-  <div class="row">
-    <div class="col-sm-10 col-sm-offset-1">
+  <footer class="container-fluid content-wrapper">
+    <div class="row">
+      <div class="col-sm-10 col-sm-offset-1">
 
-      <?php get_template_part('footer_widgets'); ?>
+        <?php get_template_part('footer_widgets'); ?>
 
+      </div>
     </div>
-  </div>
-</footer>
-<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </footer>
 </body>
 </html>
